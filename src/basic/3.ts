8e6b7247a0f9c449160b49444c6e52257ca805b3
Tinadin <pitynia8717@gmail.com>
2025-04-26 02:44:02 +0300

    Initial commit

---
+++ src/basic/3.ts
@@ -0,0 +1,6 @@
+let union: string | number;
+let literal: 'enable' | 'disable';
+
+// Створіть змінну, яка може містити або рядок, або число(union type) ?
+// Також, оголосіть змінну, яка може містити лише одне з двох можливих рядкових значень:
+// 'enable' або 'disable'(literal type).
---
+++ src/generics/1.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+async function fetchData<T>(url: string): Promise<T> {
+  try {
+    const response = await axios.get<T>(url); // типізуємо відповідь axios
+    return response.data;
+  } catch (error: any) { // додаємо тип для error
+    throw new Error(`Error fetching from ${url}: ${error.message || error}`);
+  }
+}
+
+
+// Типізуйте асинхронну функцію fetchData,
+// яка приймає URL ресурсу та повертає об'єкт з даними. 
+// Використовуйте Generics для типізації повернутих даних.
+
+// import axios from 'axios';
+// async function fetchData(url) {
+//   try {
+//     const response = await axios.get(url);
+//     return response.data;
+//   } catch (error) {
+//     throw new Error(`Error fetching from ${url}: ${error}`
